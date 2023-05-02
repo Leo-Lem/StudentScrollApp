17ea63e9eece791b7f0b7bf03a3cb23e94aa3b3c
Leopold Lemmermann <leopold@lemmermann.com>
2023-05-02 13:51:30 +1200

    Added mocked API response for posts.

--- src/api/mockAPI.ts
+++ src/api/mockAPI.ts
@@ -1,6 +1,6 @@
 import { createServer } from "miragejs"
 
-export default function () {
+export default function mockAPI(): void {
   createServer({
     routes() {
       this.post("/api/v1/posts", (_, req) => req.requestBody)
