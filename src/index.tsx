3e527c78aff7340fb12f06f2c40e748e025bb40a
Leopold Lemmermann <leopold@lemmermann.com>
2023-05-02 19:52:13 +1200

    fixing weird git bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 
-import App from "./app"
+import App from "./app1"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
