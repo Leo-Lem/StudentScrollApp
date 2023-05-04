166a4e95fa2509a8b1592b2028270cd922da7dab
glenn <117357570+Glennpgp@users.noreply.github.com>
2023-05-04 13:58:23 +1200

    added avatar for profile customization
    added a avatar component to the profile page for extra customization for the user

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,38 +1,57 @@
-import { Grid, TextField } from "@mui/material"
-import { useState, type ReactElement } from "react"
+import { Button, Grid, TextField, Avatar, Box, Stack } from "@mui/material"
+import React, { useState, type ReactElement } from "react"
+
+const chooseAvatar = ["https://i.pravatar.cc/150?img=1", "https://i.pravatar.cc/150?img=2"]
 
 export default function ProfilePage(): ReactElement {
   const [name, setName] = useState("")
   const [bio, setBio] = useState("")
 
   return (
-    <Grid container direction="column" > 
-<Grid xs={4}>
-      <TextField
-        fullWidth
-        placeholder="name"
-        value={name}
-        onChange={({ target: { value } }) => {
-          const title = value.trim()
-          setName(title)
-        }}
-      />
+    <Box flexDirection="column">
+      <Stack spacing={2} margin={5}>
+      <Grid xs={10}>
+        <TextField
+          id="user-name"
+          fullWidth
+          maxRows={4}
+          placeholder="Name"
+          value={name}
+          onChange={({ target: { value } }) => {
+            const title = value.trim()
+            setName(title)
+          }}
+        />
+        <Grid container direction="column">
+          <Grid xs={10}>
+            <TextField
+              id="user-bio"
+              fullWidth
+              maxRows={4}
+              placeholder="Bio"
+              value={bio}
+              onChange={({ target: { value } }) => {
+                const title = value.trim()
+                setBio(title)
+              }}
+            />
+          </Grid>
+          <Grid>
+            <Button color="secondary" type="submit" className="{classes.submitButton}">
+              SAVE AND UPDATE
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid container direction="column">
-      <Grid xs={4}>
-      <TextField
-        fullWidth
-        placeholder="userBio"
-        value={bio}
-        onChange={({ target: { value } }) => {
-          const title = value.trim()
-          setBio(title)
-        }}
-      />
-      </Grid>
-      
-    </Grid>
-    </Grid>
 
+      <Grid container alignContent={"center"} direction="row" spacing={4}>
+        {chooseAvatar.map((avatarLink, index) => (
+          <Grid item key={index}>
+            <Avatar src={avatarLink} alt={`Avatar ${index}`} style={{ width: 80, height: 80 }} />
+          </Grid>
+        ))}
+      </Grid>
+      </Stack>
+    </Box>
   )
 }
