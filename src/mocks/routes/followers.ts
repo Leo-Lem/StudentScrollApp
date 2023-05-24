ba2268abe6740dc065850a354eb0a158ceaf06f8
Leopold Lemmermann <leopold@lemmermann.com>
2023-05-24 18:55:59 +1200

    Updated the mocks.

--- src/mocks/routes/followers.ts
+++ src/mocks/routes/followers.ts
@@ -1,23 +1,6 @@
 import { Response, Server } from "miragejs"
 
-interface Follower {
-  studentId: number
-  followerId: number
-}
-
 export default function mock(server: Server) {
-  server.get("students/:studentId/followers", (schema, { params: { studentId } }) => {
-    return schema.db.profiles
-      .findBy({ studentId })
-      .followers.map((id: string) => schema.db.profiles.findBy({ studentId: id }))
-  })
-
-  server.get("students/:studentId/follows", (schema, { params: { studentId } }) => {
-    return schema.db.profiles
-      .findBy({ studentId })
-      .follows.map((id: string) => schema.db.profiles.findBy({ studentId: id }))
-  })
-
   server.post("students/:studentId/followers", (schema) => {
     return new Response(201)
   })
--- src/mocks/routes/profiles.ts
+++ src/mocks/routes/profiles.ts
@@ -1,15 +1,16 @@
 import { Server } from "miragejs"
+import StudentLocation from "../../features/nearby/types/StudentLocation"
 
 export default function mock(server: Server) {
   server.get("students/:studentId", async (schema: any, { url }) => {
-    return JSON.stringify(schema.profiles.findBy({ id: parseInt(url.split("/")[4]) })?.attrs)
+    return respond(schema.profiles.findBy({ id: parseInt(url.split("/")[4]) })?.attrs)
   })
 
   server.get("students", async (schema: any, { queryParams }) => {
     const lat = parseFloat(queryParams.lat)
     const lng = parseFloat(queryParams.lng)
 
-    if (!isNaN(lat) && !isNaN(lng)) return schema.profiles.all().models
+    if (!isNaN(lat) && !isNaN(lng)) return schema.profiles.all().models.map(respond)
   })
 
   server.put("students", async (schema: any, { requestBody }) => {
@@ -24,6 +25,30 @@ export default function mock(server: Server) {
       location: newLocation ?? profile.location
     })
 
-    return JSON.stringify(profile.attrs)
+    return respond(profile.attrs)
   })
+}
+
+interface Response {
+  studentId: number
+  name: string
+  bio: string
+  icon: string
+  interests: string[]
+  location: StudentLocation
+  followers: number[]
+  follows: number[]
+}
+
+function respond(profile: any): Response {
+  return {
+    studentId: parseInt(profile.studentId),
+    name: profile.name,
+    bio: profile.bio,
+    icon: profile.icon,
+    interests: profile.interests,
+    location: profile.location,
+    followers: profile.followers.map((follower: any) => parseInt(follower.studentId)),
+    follows: profile.follows.map((follow: any) => parseInt(follow.studentId))
+  }
 }
